1bf0ebf4c950bafe327f0f3419bfe2114367f69c
Anthony Yunusa <anthonyyunusa@gmail.com>
2020-10-19 03:26:59 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,14 +6,19 @@ namespace CaesarCipher
   {
     static void Main(string[] args)
     {
+      //declare an array of alphabets
       char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
+      
+      // Request message from user and store it in a variable.
       Console.WriteLine("Type in your message");
       string input = Console.ReadLine();
       char[] secretMessage = input.ToCharArray();
       
+      // Declare two new empty array to hold the cypered and decyphered messages
       char[] encryptedMessage = new char[secretMessage.Length];
       char [] decryptedMessage = new char[secretMessage.Length];
+      
+      //To Cypher
       for(int i = 0; i < secretMessage.Length; i++)
       {
         char cypherLetter = secretMessage[i];
@@ -26,8 +31,9 @@ namespace CaesarCipher
 
       }
       string inComingMessage = String.Join("", encryptedMessage);
-        Console.WriteLine(inComingMessage);
+        Console.WriteLine($"The decyphered message is: {inComingMessage}");
     
+      //To Decypher
     for(int i = 0; i < inComingMessage.Length; i++)
       {
         char decypherLetter = inComingMessage[i];
@@ -41,7 +47,7 @@ namespace CaesarCipher
       }
 
       string decypherMessage = String.Join("", decryptedMessage);
-        Console.WriteLine(decypherMessage);
+        Console.WriteLine($"The decyphered message is: {decypherMessage}");
     }
 
   }
